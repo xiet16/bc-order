db73312e50474c986070c1f1f3a36accf7b14e81
xiet16 <13249894151@163.com>
2023-04-11 22:44:59 +0800

    添加get接口

--- handler/demo.go
+++ handler/demo.go
@@ -11,8 +11,13 @@ func NewDemo() *demo {
 
 func (a *demo) InitRouter(router *gin.Engine) {
 	router.POST("/demo", a.demo)
+	router.GET("/hello", a.hello)
 }
 
 func (a *demo) demo(c *gin.Context) {
 	c.JSON(200, gin.H{"msg": "hello xiet demo"})
+}
+
+func (a *demo) hello(c *gin.Context) {
+	c.JSON(200, gin.H{"msg": "hello xiet"})
 }
